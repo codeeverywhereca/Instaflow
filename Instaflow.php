2575991047a9a13896ba5727e08110d5ecfbd7cc
codeeverywhereca <codeeverywhereca@users.noreply.github.com>
2018-01-02 13:21:13 -0500

    Update Instaflow.php

--- Instaflow.php
+++ Instaflow.php
@@ -1,33 +1,34 @@
 <?php
+
 /*
- * Instaflow.php (Dec 30 2017)
+ * Instaflow.php (Jan 2 2017)
  * https://github.com/codeeverywhereca/Instaflow
  * Copyright 2017, http://codeeverywhere.ca
  * Licensed under the MIT license.
  */
+	
+$username = "instagram";
 
-if( isset($_GET['user']) && !empty($_GET['user']) ) {
+if( !file_exists('insta.cache') || (time() - filemtime('insta.cache') > 60 * 60 * 6) ) {
 	
-	if( !file_exists('insta.cache') || (time() - filemtime('insta.cache') > 60 * 60 * 3) ) {
-		
-		$html = file_get_contents('https://www.instagram.com/' . $_GET['user']);
-		$regex = '/<script type="text\/javascript">window\._sharedData = {(.+)};<\/script>/i';
-		
-		if( !preg_match($regex, $html, $matches) ) {
-			echo '{ "status" : false, "message" : "no matches" }';
-		} else {
-			$json = json_decode( '{' . $matches[1] . '}' );
-			echo $json = json_encode( $json->entry_data->ProfilePage[0]->user );
-			file_put_contents('insta.cache', $json);
-		}
+	$html = file_get_contents('https://www.instagram.com/' . $username);
+	$regex = '/<script type="text\/javascript">window\._sharedData = {(.+)};<\/script>/i';
 	
+	if( !preg_match($regex, $html, $matches) ) {
+		
+		echo '{ "status" : false, "message" : "no matches" }';
+		
 	} else {
 		
-		echo file_get_contents('insta.cache');
+		$json = json_decode( '{' . $matches[1] . '}' );
+		echo $json = json_encode( $json->entry_data->ProfilePage[0]->user );
+		
+		file_put_contents('insta.cache', $json);
 	}
-	
-} else {
-	echo '{ "status" : false, "message" : "no username provided" }';
-}
 
+} else {
+	
+	echo file_get_contents('insta.cache');
+
+}
 ?>
