7cb5df06a14fb376a1412c4856aea49a9ee38054
codeeverywhereca <codeeverywhereca@users.noreply.github.com>
2018-03-18 00:13:22 -0400

    Update Instaflow.js

--- Instaflow.js
+++ Instaflow.js
@@ -30,15 +30,15 @@
 	
 		// Draw divs on page
 		this.draw = function(json) {
-			var buffer = '', json = JSON.parse(json);
-			for( var index = 0; index < json.media.nodes.length && index < 15; index++ ) {
+			var buffer = '', media = JSON.parse(json);
+			for( var index = 0; index < media.length && index < 15; index++ ) {
 				
 				var
-					node = json.media.nodes[index],
-					d = new Date(node.date * 1000).toString(),
+					node = media[index].node,
+					d = new Date(node.taken_at_timestamp * 1000).toString(),
 					date = d.substr(4,6) + ', ' + d.substr(16,5); date.substr(0,10),
-					likes = node.likes.count,
-					link = `https://www.instagram.com/p/${node.code}/`
+					likes = node.edge_liked_by.count,
+					link = `https://www.instagram.com/p/${node.shortcode}/`
 				;
 				
 				// shorten like count
@@ -50,7 +50,7 @@
 				var template = `<a href="${link}">
 					<div class="title">${likes} Likes <span class="grey">${date}</span></div>
 					<div class="image" style="background-image:url(${node.thumbnail_resources[3].src})"></div>
-					<div class="text">${node.caption}</div>
+					<div class="text">${node.edge_media_to_caption.edges[0].node.text}</div>
 				</a>`;
 				buffer += template;
 			}
